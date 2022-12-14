10e72de57e218ce93ad023a6df377506d837eedd
Sergio <sergiohg818@gmail.com>
2022-12-14 19:58:11 +0100

    casi consulta

--- createMongoDBFromPostgreSQLDB.py
+++ createMongoDBFromPostgreSQLDB.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import ast
 import os
 import sys, traceback
 from sqlalchemy import and_, create_engine, update
@@ -38,31 +38,12 @@ def getImdb_UKmovietitle():
         return 'Something is broken'
 
 
-def getImdb_UKmoviegenres(): 
+def getImdb_UKtitle2actors(): 
    
     try:
         db_conn = None
         db_conn = db_engine.connect()
-        db_result = db_conn.execute("Select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title,  mg.genre from imdb_moviegenres mg join imdb_movies mo on mg.movieid = mo.movieid where mg.movieid  in (Select mo.movieid from imdb_movies mo join imdb_moviecountries im ON im.country='UK' and im.movieid = mo.movieid order by year desc limit 400) order by mo.year desc, title desc ;")
-
-        return db_result.fetchall()
-    except:
-        if db_conn is not None:
-            db_conn.close()
-        print("Exception in DB access:")
-        print("-"*60)
-        traceback.print_exc(file=sys.stderr)
-        print("-"*60)
-
-        return 'Something is broken'
-
-
-def getImdb_UKmovietitlegenre(): 
-   
-    try:
-        db_conn = None
-        db_conn = db_engine.connect()
-        db_result = db_conn.execute("Select mg.genre, mg.movieid from imdb_moviegenres mg where mg.movieid  in (Select mo.movieid from imdb_movies mo join imdb_moviecountries im ON im.country='UK' and im.movieid = mo.movieid order by year desc)")
+        db_result = db_conn.execute("Select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title, STRING_AGG(concat('\"',mg.genre, '\"'), ', ') as genres, mo.year, mo.ratingcount, mo.ratingmean, x.directors, y.actors from imdb_moviegenres mg join imdb_movies mo on mg.movieid = mo.movieid join ( select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title2, STRING_AGG(concat('\"' , md.directorname , '\"'), ', ') as directors from imdb_directormovies dm join imdb_movies mo on mo.movieid = dm.movieid join imdb_directors md on dm.directorid = md.directorid group by mo.year, mo.movieid order by mo.year desc) as x on replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') = x.title2 join( select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title2, STRING_AGG(concat('\"', ma.actorname , '\"'), ', ') as actors from imdb_actormovies am join imdb_movies mo on mo.movieid = am.movieid join imdb_actors ma on am.actorid = ma.actorid group by mo.year, mo.movieid order by mo.year desc) as y on replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') = y.title2 where mg.movieid  in ( Select mo.movieid from imdb_movies mo join imdb_moviecountries im ON im.country='UK' and im.movieid = mo.movieid order by year desc limit 400) group by title, mo.year , mo.movietitle, x.directors, mo.ratingcount, mo.ratingmean, y.actors order by mo.year desc, title desc;")
 
         return db_result.fetchall()
     except:
@@ -82,30 +63,33 @@ def getImdb_UKmovietitlegenre():
 if __name__ == "__main__":
 
 
-    print('Generating imdb_moviesname.json')
-    query = getImdb_UKmovietitle()
+    print('Generating title2actors.json')
+    query = getImdb_UKtitle2actors()
     newUk = []
     for row in query:
         d = dict()
-        d['movietitle'] = row[0]
+        d['title'] = row[0]
+
+        d['genres'] = [str(row[1])]
+        d['genres']= str(d['genres']).replace("'", "")
+        d['genres']= str(d['genres']).replace("\\", "")
+        d['genres'] = ast.literal_eval(d['genres'])
+        
+        d['year'] = row[2]
+        d['number_of_votes'] = row[3]
+        d['average_rating'] = row[4]
+        d['directors'] = [str(row[5])]
+        d['directors']= str(d['directors']).replace("'", "")
+        d['directors']= str(d['directors']).replace("\\", "")
+        d['directors'] = ast.literal_eval(d['directors'])
+        d['actors'] = [str(row[6])]
+        d['actors']= str(d['actors']).replace("'", "")
+        d['actors']= str(d['actors']).replace("\\", "")
+        d['actors'] = ast.literal_eval(d['actors'])
+        
         newUk.append(d)
-    with open('mongojs/imdb_moviesname.json', 'w') as f:
-        f.write(json.dumps(newUk, indent = 4))
 
         
-    print('Generating imdb_genres.json')
-    query = getImdb_UKmoviegenres()
-    daux=[]
-    aux=query[0][1]
-    for row in query:
-        d = dict()
-        if(row[1] == aux):
-            daux.append(row[0])
-        else:
-            d['genre'] = daux
-            newUk.append(d)
-        aux=row[1]
-    with open('mongojs/imdb_genres.json', 'w') as f:
-        f.write(json.dumps(newUk, indent = 4))
-    print(newUk)
-
+    with open('mongojs/title2actors.json', 'w') as f:
+        f.write(json.dumps(newUk, indent = 5))
+   
--- uk.sql
+++ uk.sql
@@ -1,14 +1,14 @@
-Select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title, STRING_AGG(mg.genre, ', ') as genres, 
-mo.year, mo.ratingmean, x.directors, y.actors
+Select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title, STRING_AGG(concat('\"',mg.genre, '\"'), ', ') as genres, 
+mo.year, mo.ratingcount, mo.ratingmean, x.directors, y.actors
 from imdb_moviegenres mg join imdb_movies mo on mg.movieid = mo.movieid 
 join (
-	select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title2, STRING_AGG(md.directorname, ', ') as directors
+	select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title2, STRING_AGG(concat('\"' , md.directorname , '\"'), ', ') as directors
 	from imdb_directormovies dm join imdb_movies mo on mo.movieid = dm.movieid join imdb_directors md on dm.directorid = md.directorid
 	group by mo.year, mo.movieid 
 	order by mo.year desc) as x
 on replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') = x.title2
 join(
-    select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title2, STRING_AGG(ma.actorname, ', ') as actors
+    select replace(substring(mo.movietitle,1, length(mo.movietitle) - 7), '(', '') as title2, STRING_AGG(concat('\"', ma.actorname , '\"'), ', ') as actors
 	from imdb_actormovies am join imdb_movies mo on mo.movieid = am.movieid join imdb_actors ma on am.actorid = ma.actorid
 	group by mo.year, mo.movieid 
 	order by mo.year desc) as y 
@@ -17,7 +17,7 @@ where mg.movieid  in (
     Select mo.movieid from imdb_movies mo 
     join imdb_moviecountries im ON im.country='UK' and im.movieid = mo.movieid 
     order by year desc limit 400) 
-group by title, mo.year , mo.movietitle, x.directors, mo.ratingmean, y.actors
+group by title, mo.year , mo.movietitle, x.directors, mo.ratingcount, mo.ratingmean, y.actors
 order by mo.year desc, title desc; 
 
 
@@ -44,4 +44,4 @@ group by mo.year, mo.movieid
 order by mo.year desc;
 
 
-
+-------
